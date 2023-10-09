62cbc0fe18b8c09435737a1dab4a47cb1259d3b7
NeiltonSeguins <seguins.neilton@gmail.com>
2023-10-09 19:07:47 -0300

    Animação com JavaScript. Animação de bounce de uma outra forma. SEGUNDA FORMA

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const imagemFlutuante = document.querySelector(".imagemFlutuante");
-let direcao = 1; // Define a direção da flutuação
-let distancia = 10; // Define a distância vertical da flutuação em pixels
 
 function animarFlutuacao() {
-  imagemFlutuante.style.transform = `translateY(${distancia}px)`;
+  let distancia = 20; // Distância vertical da flutuação em pixels
+  let duracao = 2000; // Duração da animação em milissegundos
 
-  // Inverte a direção quando atinge um limite
-  if (distancia === 10) {
-    direcao = -1;
-  } else if (distancia === -10) {
-    direcao = 1;
-  }
+  imagemFlutuante.style.transition = `transform ${duracao}ms ease-in-out`;
+  imagemFlutuante.style.transform = `translateY(-${distancia}px)`;
 
-  distancia += direcao;
+  setTimeout(() => {
+    imagemFlutuante.style.transition = `transform ${duracao}ms ease-in-out`;
+    imagemFlutuante.style.transform = `translateY(${distancia}px)`;
+
+    setTimeout(() => {
+      animarFlutuacao();
+    }, duracao);
+  }, duracao);
 }
 
-// Iniciar a animação a cada 200 milissegundos
-setInterval(animarFlutuacao, 45);
+// Iniciar a animação quando a página carregar
+window.addEventListener("load", animarFlutuacao);
